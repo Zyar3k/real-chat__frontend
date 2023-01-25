8058a50f4e4dcb92f3528d3bdd6bb2f783788ac2
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-25 18:41:15 +0100

    Add readme

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8888";
+export const host = "https://chat-app-server-ez0p.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
